e3540b1149e4d69f2aac15f2dcfe44db86f8c72f
Arthur Teixeira Serafim <arthurteixeiraserafim@gmail.com>
2025-06-12 09:06:26 -0300

    Mudanças de Reutilização de componentes
    Mudanças de Reutilização de componentes e layout

--- src/Pages/Modalitys/index.tsx
+++ src/Pages/Modalitys/index.tsx
@@ -2,7 +2,6 @@ import { TopBar } from "../../components/TopBar";
 import { SideMenu} from "../../components/SideMenu";
 import { MainContentContainer } from "../../components/utils/generic";
 import { NewRegister } from "../../components/NewRegister";
-import { CustomerCard } from "../../components/AlunoCard";
 
 export function Modalitys() {
     return(
@@ -10,9 +9,8 @@ export function Modalitys() {
         <TopBar placeholder="pesquisar por modalidades"></TopBar>
         <SideMenu></SideMenu>
         <MainContentContainer $repeatColumns={4}> 
-               <NewRegister entityName="Modalidade" createPath="/newmodality" buttonText="Nova modalidade"/>
-              </MainContentContainer>
-
+            <NewRegister entityName="Modalidade" createPath="/newmodality" buttonText="Nova modalidade"/>
+        </MainContentContainer>
         </>
     )
 }
